8817d33826f1baba203d47a14f89ce0c4905685b
Prerna <Prernagour2904@gmail.com>
2025-07-17 02:46:55 +0530

    writting-auth-middleware

--- src/app.js
+++ src/app.js
@@ -1,13 +1,22 @@
 const express = require("express");
+const { adminAuth } = require("./middlewares/auth");
 
 const app = express();
 
-app.use("/test",(req,res)=>{
-    res.send("Hello From Server");
-})
+app.use("/admin", adminAuth);
 
-app.listen(3000,()=>{
-    console.log("Server is Succesfully Listening");
+app.get("/admin/getAllData", (req, res) => {
+    res.send("Get All Data");
 });
 
+app.get("/admin/getAll", (req, res) => {
+    res.send("Get All route is working");
+});
 
+app.get("/user", (req, res) => {
+    res.send("Yes Working 1");
+});
+
+app.listen(3000, () => {
+    console.log("Server is Successfully Listening");
+});
