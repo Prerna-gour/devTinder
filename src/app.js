db5fd3c4bd3f8dabdb9f2f878d6dc2754cc37444
Prerna <Prernagour2904@gmail.com>
2025-07-21 14:51:19 +0530

    validator-library

--- src/app.js
+++ src/app.js
@@ -2,24 +2,68 @@ const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
 const User = require("./models/user");
+const user = require("./models/user");
 
-app.post("/signup",async(req,res)=>{
+app.use(express.json());
+
+app.post("/signup", async (req, res) => {
     //Creating a new Instance of the User model
-    const user = new User({
-        firstName:"Prerna",
-        lastName:"Gour",
-        emailId:"Prernagour2904@gmail.com",
-        password:"Prerna@2904",
-    })
+    const user = new User(req.body)
 
-   try{
-     await user.save();
-     res.send("Data Added Sucesfully");
-   }
-   catch{
-    res.send(400).send("API Not Working");
-   }
-})
+    try {
+        await user.save();
+        res.send("Data Added Sucesfully");
+    }
+    catch {
+        res.send(400).send("API Not Working");
+    }
+});
+
+app.get("/user", async (req, res) => {
+    const userEmail = req.body.emailId;
+    console.log(User.emailId);
+    try {
+
+        const user = await User.find({ emailId: userEmail });
+        res.send(user)
+    }
+    catch {
+        res.status(400).send("Something went wrong");
+    }
+});
+
+app.get("/feed", async (req, res) => {
+    try {
+        const users = await user.find({});
+        res.send(users);
+    }
+    catch (err) {
+        res.status(400).send("Not Getting Feed Data");
+    }
+});
+
+app.delete("/user", async (req, res) => {
+    const userId = req.body.userId;
+    try {
+        const user = await User.findByIdAndDelete(userId);
+        res.send("User Deleted");
+    }
+    catch (err) {
+        res.status(400).send("User Not Deleted");
+    }
+});
+
+app.patch("/user", async(req,res)=>{
+    const userId = req.body.userId;
+    const data = req.body;
+    try{
+        await User.findByIdAndUpdate(userId, data,{runValidators:true});
+        res.send("User Updated");
+    }
+    catch(err){
+        res.status(400).send("Something Went Wrong"+ err);
+    }
+});
 
 connectDB()
     .then(() => {
--- src/models/user.js
+++ src/models/user.js
@@ -1,26 +1,67 @@
 const mongoose = require("mongoose");
+const validator  = require("validator");
 
 const userSchema = new mongoose.Schema({
     firstName: {
-        type: String
+        type: String,
+        required: true
     },
     lastName: {
-        type:String
+        type: String
     },
-    emailId: {
-        type:String
+    emailId: { 
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error("Invalid Mail Address");
+            }
+        }
     },
     password: {
-        type: String
+        type: String,
+        required: true,
+        validator(value){
+            if(!validator.isStrongPassword(value)){
+                throw new Error("Enter Strong Password"+ value);
+            }
+        }
     },
     age: {
-        type: String
+        type: String,
     },
     gender: {
-        type: String
+        type: String,
+        validate(value) {
+            if (!["male", "female", "others"].includes(value)) {
+                throw new Error("Gender Data is not Valid");
+            }
+        }
+    },
+    photoUrl: {
+        type: String,
+        default: "https://www.google.com/imgres?q=dummy%20user%20image&imgurl=https%3A%2F%2Fsriit.ac.in%2Ftool%2Fplugins%2Fimages%2Fusers%2F4.jpg&imgrefurl=https%3A%2F%2Fsriit.ac.in%2Ftool%2Fplugins%2Fimages%2Fusers%2F&docid=1G1F0UzseHikKM&tbnid=DhFnQfv70T5ClM&vet=12ahUKEwj5gfju9MqOAxVXRmcHHRdqBYAQM3oECH8QAA..i&w=600&h=600&hcb=2&ved=2ahUKEwj5gfju9MqOAxVXRmcHHRdqBYAQM3oECH8QAA",
+        validator(value){
+            if(!validator.isURL(value)){
+                throw new Error("Photo URL is not Correct");
+            }
+        }
+    },
+    about: {
+        type: String,
+        default: "This is Default about of the Person"
+    },
+    skills: {
+        type: [String]
+        //Because there are multiple Skills
     }
+
+}, {
+    timestamps: true
 });
 
 //(Name of model, schema);
 
-module.exports = mongoose.model("User",userSchema);
+module.exports = mongoose.model("User", userSchema);
